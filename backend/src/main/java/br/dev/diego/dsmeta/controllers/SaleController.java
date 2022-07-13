ebde8ef70ec92b8869ddc5a587c279c4af213b4d
Eder Diego de Sousa <me@diego.dev.br>
2022-07-13 18:05:42 -0300

    Twillio SMS

--- backend/src/main/java/br/dev/diego/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/br/dev/diego/dsmeta/controllers/SaleController.java
@@ -2,11 +2,13 @@ package br.dev.diego.dsmeta.controllers;
 
 import br.dev.diego.dsmeta.controllers.dto.SaleRespondeDto;
 import br.dev.diego.dsmeta.services.SaleService;
+import br.dev.diego.dsmeta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +20,21 @@ public class SaleController {
     @Autowired
     private SaleService service;
 
+    @Autowired
+    private SmsService smsService;
+
     @GetMapping
     public ResponseEntity<Page<SaleRespondeDto>> findSales(
             @RequestParam(value = "minDate", defaultValue = "") String minDate,
             @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
             Pageable pageable) {
         return ResponseEntity.ok(service.findSales(minDate, maxDate, pageable));
+    }
+
+    @GetMapping("/{id}/notification")
+    public ResponseEntity<Void> notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
---
+++ backend/src/main/java/br/dev/diego/dsmeta/services/SmsService.java
@@ -0,0 +1,64 @@
+package br.dev.diego.dsmeta.services;
+
+import br.dev.diego.dsmeta.entities.Sale;
+import br.dev.diego.dsmeta.repositories.SaleRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+@Service
+public class SmsService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SmsService.class);
+    @Autowired
+    private SaleRepository saleRepository;
+
+    @Value("${twilio.sid}")
+    private String twilioSid;
+
+    @Value("${twilio.key}")
+    private String twilioKey;
+
+    @Value("${twilio.phone.from}")
+    private String twilioPhoneFrom;
+
+    @Value("${twilio.phone.to}")
+    private String twilioPhoneTo;
+
+    public void sendSms(Long id) {
+
+        Twilio.init(twilioSid, twilioKey);
+
+        PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+        PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+        Sale sale = saleRepository.findById(id).orElseThrow(() ->
+                new EntityNotFoundException("Venda não encontrada"));
+        String msg = "Data: " + brDate(sale.getDate()) +
+                        "\nVendedor: " + sale.getSellerName() +
+                        "\nValor: " + brValue(sale.getAmount());
+
+        Message message = Message.creator(to, from, msg).create();
+        LOG.info(">>>>Sending sms... >>> Message id: {}", message.getSid());
+    }
+
+    private String brDate(LocalDate localDate) {
+        return localDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
+
+    private String brValue(Double value) {
+        Locale ptBR = new Locale("pt", "BR");
+        return NumberFormat.getCurrencyInstance(ptBR).format(value);
+    }
+}
