2f22391810c0b1e973ce2319ceda516465852313
Eder Diego de Sousa <me@diego.dev.br>
2022-07-13 16:47:40 -0300

    Date Select

--- backend/src/main/java/br/dev/diego/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/br/dev/diego/dsmeta/controllers/SaleController.java
@@ -3,12 +3,13 @@ package br.dev.diego.dsmeta.controllers;
 import br.dev.diego.dsmeta.controllers.dto.SaleRespondeDto;
 import br.dev.diego.dsmeta.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -18,8 +19,11 @@ public class SaleController {
     private SaleService service;
 
     @GetMapping
-    public ResponseEntity<List<SaleRespondeDto>> findALl() {
-        return ResponseEntity.ok(service.findAll());
+    public ResponseEntity<Page<SaleRespondeDto>> findSales(
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+            Pageable pageable) {
+        return ResponseEntity.ok(service.findSales(minDate, maxDate, pageable));
     }
 
 }
--- backend/src/main/java/br/dev/diego/dsmeta/services/SaleService.java
+++ backend/src/main/java/br/dev/diego/dsmeta/services/SaleService.java
@@ -3,11 +3,14 @@ package br.dev.diego.dsmeta.services;
 import br.dev.diego.dsmeta.controllers.dto.SaleRespondeDto;
 import br.dev.diego.dsmeta.repositories.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 @Service
 public class SaleService {
@@ -15,7 +18,14 @@ public class SaleService {
     @Autowired
     private SaleRepository repository;
     @Transactional
-    public List<SaleRespondeDto> findAll() {
-        return repository.findAll().stream().map(SaleRespondeDto::new).collect(Collectors.toList());
+    public Page<SaleRespondeDto> findSales(String min, String max, Pageable pageable) {
+
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        LocalDate minDate = min.equals("") ? today.minusDays(365) : LocalDate.parse(min);
+        LocalDate maxDate = max.equals("") ? today : LocalDate.parse(max);
+
+        return repository.findSales(minDate, maxDate, pageable).map(SaleRespondeDto::new);
     }
+
 }
